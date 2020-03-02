f597659edb3bcf7b3732482ab22ad68940e4590b
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-03-02 15:31:46 +0100

    Testing

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,7 +12,7 @@ PREV_COMMIT="$(git rev-parse --verify HEAD~1)"
 for file in $(ls -p | grep -v /); do
   if [ $(jq --arg file "$file" -r '.[] | select(.configuration == $file) | .commitHash' "$ROOTDIR/tmp-register.json") ]
   then
-    UPDATED_OBJECT=$(jq --arg file "$file" --arg CURRENT_COMMIT "$CURRENT_COMMIT" -r '.[] | select(.configuration == $file) | .commitHash = $CURRENT_COMMIT' "$ROOTDIR/tmp-register.json" | jq .)
+    UPDATED_OBJECT=$(jq --arg file "$file" --arg CURRENT_COMMIT "$CURRENT_COMMIT" -r '.[] | select(.configuration == $file) | .commitHash = $CURRENT_COMMIT' "$ROOTDIR/tmp-register.json")
     echo "RESULT: "
     echo "$UPDATED_OBJECT"
     jq --arg UPDATED_OBJECT "$UPDATED_OBJECT" '. |= .+ [$UPDATED_OBJECT]' "$ROOTDIR/tmp-register.json" > "$ROOTDIR/tmp.json"
@@ -20,7 +20,6 @@ for file in $(ls -p | grep -v /); do
 
 ##TODO: UPDATED OBJECT SHOULD BE FORMATTED
 
-    cat "$ROOTDIR/tmp-register.json"
     echo "$file was changed and is present"
   else
     echo "$file was changed and not present"
