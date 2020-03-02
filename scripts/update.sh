3b04300f331153d64480d43f7db2644fcfda8534
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-03-02 13:10:01 +0100

    update

--- scripts/update.sh
+++ scripts/update.sh
@@ -10,7 +10,6 @@ PREV_COMMIT="$(git rev-parse --verify HEAD~1)"
 
 
 for file in $(ls -p | grep -v /); do
-  jq --arg file "$file" -r -c '.[] | select(.configuration == $file) | .commitHash' /tmp/
 workspace/tmp-register.json
   if [ $(jq --arg file "$file" -r '.[] | select(.configuration == $file) | .commitHash' "$ROOTDIR/tmp-register.json") -ne "" ]
   then
