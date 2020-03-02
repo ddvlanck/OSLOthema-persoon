5ac84bd6cb13ff0a13d0df4207f930075153832c
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-03-02 14:05:13 +0100

    Testing

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,10 +12,13 @@ PREV_COMMIT="$(git rev-parse --verify HEAD~1)"
 for file in $(ls -p | grep -v /); do
   if [ $(jq --arg file "$file" -r '.[] | select(.configuration == $file) | .commitHash' "$ROOTDIR/tmp-register.json") ]
   then
-    UPDATED_OBJECT=$(jq --arg file "$file" -r '.[] | select(.configuration == $file) | .commitHash = "$CURRENT_COMMIT"' "$ROOTDIR/tmp-register.json")
+    UPDATED_OBJECT=$(jq --arg file "$file" -r '.[] | select(.configuration == $file) | .commitHash = "$CURRENT_COMMIT"' "$ROOTDIR/tmp-register.json" | jq .)
+    echo "RESULT: "
+    echo "$UPDATED_OBJECT"
     jq --arg object "$UPDATED_OBJECT" '. |= .+ [$object]' "$ROOTDIR/tmp-register.json" > "$ROOTDIR/tmp.json"
     cat "$ROOTDIR/tmp.json"
 
+##TODO: UPDATED OBJECT SHOULD BE FORMATTED
 
     cat "$ROOTDIR/tmp-register.json"
     echo "$file was changed and is present"
