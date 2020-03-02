2697da8076afc5143036f49348a957b6e6aa714b
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-03-02 11:56:16 +0100

    update

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,17 +3,16 @@ ROOTDIR=$1
 
 mkdir -p "$ROOTDIR"
 
-curl -o /tmp/workspace/tmp-register.json https://raw.githubusercontent.com/ddvlanck/OSLO-StandaardenGeneral/master/standaardenregister.json
+curl -o "$ROOTDIR/tmp-register.json" https://raw.githubusercontent.com/ddvlanck/OSLO-StandaardenGeneral/master/standaardenregister.json
 
 CURRENT_COMMIT="$(git rev-parse --verify HEAD)"
-PREV_COMMIT="$(git log -n2 --format=format:"%H")"
+PREV_COMMIT="$(git rev-parse --verify HEAD~1)"
 
 changedFiles="$(git diff --name-only "$PREV_COMMIT")"
 
 while read -r file; do
-  jq -r '.[] | select(.configuration == "vocabularium-melding.jn") | .commitHash' standaardenregister.json
 
-  if [ $(jq -r '.[] | select(.configuration == "$file") | .commitHash' standaardenregister.json) -ne "" ]
+  if [ $(jq -r '.[] | select(.configuration == "$file") | .commitHash' "$ROOTDIR/tmp-register.json") -ne "" ]
   then
     echo "$file was changed and is also used as configuration file in standards register"
   else
