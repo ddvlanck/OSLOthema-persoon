d1f46d9129ec5f6dbf6e57d69e14506a1b21aeed
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-03-02 13:22:42 +0100

    Testing

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,8 +12,8 @@ PREV_COMMIT="$(git rev-parse --verify HEAD~1)"
 for file in $(ls -p | grep -v /); do
   if [ $(jq --arg file "$file" -r '.[] | select(.configuration == $file) | .commitHash' "$ROOTDIR/tmp-register.json") ]
   then
-    jq --arg file "$file" -r '.[] | select(.configuration == $file) | .commitHash = "test1234"' "$ROOTDIR/tmp-register.json"
-    cat "$ROOTDIR/tmp-register.json"
+    jq --arg file "$file" -r '.[] | select(.configuration == $file) | .commitHash = "test1234"' "$ROOTDIR/tmp-register.json" > "$ROOTDIR/tmp.json"
+    cat "$ROOTDIR/tmp.json"
     echo "$file was changed and is present"
   else
     echo "$file was changed and not present"
