d1d85abd2da5fc07ab7db3068e29ce86c08623a5
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-03-03 15:25:48 +0100

    Updating everything final

--- scripts/update.sh
+++ scripts/update.sh
@@ -16,11 +16,7 @@ for file in $(ls -p | grep -v /); do
   if [ $(jq --arg file "$file" -r '.[] | select(.configuration == $file) | .commitHash' "$ROOTDIR/standaardenregister.json") ]
   then
     jq --arg file "$file" --arg COMMIT "$CURRENT_COMMIT" '. | map(if .configuration == $file then . + {"commitHash" : $COMMIT} else . end)' "$ROOTDIR/standaardenregister.json" > "$ROOTDIR/updated-register.json"
-    echo "Printing standards register"
-    cat "$ROOTDIR/standaardenregister.json"
-    echo "===================="
-    echo "Printing tmp file"
-    cat "$ROOTDIR/updated-register.json"
+    echo "Updated standards register for ${CIRCLE_PROJECT_REPONAME} with new commit hash: $CURRENT_COMMIT"
   else
     echo "$file is not present in the standards register"
   fi
