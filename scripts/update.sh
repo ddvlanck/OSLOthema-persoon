06eb4e5744baf81714ba5748a27d0fd27a25eb24
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-03-03 15:22:17 +0100

    Testing 2

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,14 +6,24 @@ mkdir -p "$ROOTDIR"
 curl -o "$ROOTDIR/standaardenregister.json" https://raw.githubusercontent.com/ddvlanck/OSLO-StandaardenGeneral/master/standaardenregister.json
 
 CURRENT_COMMIT="$(git rev-parse --verify HEAD)"
+PREV_COMMIT="$(git rev-parse --verify HEAD~1)"
+
+echo "Old commit hash is: $PREV_COMMIT"
+echo "New commit hash is: $CURRENT_COMMIT"
 
 
 for file in $(ls -p | grep -v /); do
   if [ $(jq --arg file "$file" -r '.[] | select(.configuration == $file)' "$ROOTDIR/standaardenregister.json") ]
   then
     jq --arg file "$file" --arg COMMIT "$CURRENT_COMMIT" '. | map(if .configuration == $file then . + {"commitHash" : $COMMIT} else . end)' "$ROOTDIR/standaardenregister.json" > "$ROOTDIR/updated-register.json"
+    echo "Printing standards register"
+    cat "$ROOTDIR/standaardenregister.json"
+    echo "===================="
+    echo "Printing tmp file"
+    cat "$ROOTDIR/updated-register.json"
   else
     echo "$file is not present in the standards register"
   fi
 done
+
 
